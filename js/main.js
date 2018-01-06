b238e5b30799039c50684ba026726a82d0e9c9ac
petermalcolm <peter.t.malcolm@gmail.com>
2018-01-06 13:45:05 -0500

    main colors are semicircles, refactor js with initPalette

--- js/main.js
+++ js/main.js
@@ -6,19 +6,26 @@
 
 (function iife(){
 	console.log('ready to mix colors! ' + colors.length);
-	var palette = document.querySelector('#palette');
-	palette.innerHTML ='<h2>Your Colors</h2>';
-	for (var i = 0; i < colors.length; i++) {
-		var el = document.createElement('div');
-		el.id = colors[i].name;
-		el.className = 'colorChoice';
-		var elName = document.createElement('span');
-		elName.innerHTML = colors[i].name;
-		var elSwatch = document.createElement('div');
-		elSwatch.className = 'colorSwatch';
-		elSwatch.style.backgroundColor = '#' + colors[i].hex;
-		el.appendChild(elName);
-		el.appendChild(elSwatch);
-		palette.appendChild(el);
+
+	function initPalette() {
+		var palette = document.querySelector('#palette');
+		palette.innerHTML ='<h2>Your Colors</h2>';
+		for (var i = 0; i < colors.length; i++) {
+			var el = document.createElement('div');
+			el.id = colors[i].name;
+			el.className = 'colorChoice';
+			var elName = document.createElement('span');
+			elName.innerHTML = colors[i].name;
+			var elSwatch = document.createElement('div');
+			elSwatch.className = 'colorSwatch';
+			elSwatch.style.backgroundColor = '#' + colors[i].hex;
+			el.appendChild(elName);
+			el.appendChild(elSwatch);
+			palette.appendChild(el);
+		}
 	}
+
+	document.addEventListener("DOMContentLoaded", function(event) { 
+		initPalette();
+	});
 }());
