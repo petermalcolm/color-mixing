1c44dad42ff4e65dc46a1f8b166c469123b6edc4
petermalcolm <peter.t.malcolm@gmail.com>
2018-01-06 10:25:19 -0500

    color palette!

--- js/_config.js
+++ js/_config.js
@@ -1,15 +1,15 @@
 'use strict';
-var colors = {
-	blue1 : {},
-	blue2 : {},
-	green1 : {},
-	green2 : {},
-	pink : {},
-	red : {},
-	orange : {},
-	yellow : {},
-	purple : {},
-	black : {},
-	white : {},
-	brown : {},
-}
+var colors = [
+	{ name: "blue1", hex : "0000AA" },
+	{ name: "blue2", hex : "AAAAFF" },
+	{ name: "green1", hex : "009900" },
+	{ name: "green2", hex : "00FF00" },
+	{ name: "pink", hex : "FF8888" },
+	{ name: "red", hex : "CC0000" },
+	{ name: "orange", hex : "FF8800" },
+	{ name: "yellow", hex : "FFFF00" },
+	{ name: "purple", hex : "AA00AA" },
+	{ name: "black", hex : "000000" },
+	{ name: "white", hex : "FFFFFF" },
+	{ name: "brown", hex : "883300" },
+]
--- js/main.js
+++ js/main.js
@@ -2,4 +2,23 @@
  * Main JavaScript for the Color Mixing App
  */
 
- console.log('ready to mix colors!');
+'use strict';
+
+(function iife(){
+	console.log('ready to mix colors! ' + colors.length);
+	var palette = document.querySelector('#palette');
+	palette.innerHTML ='<h2>Your Colors</h2>';
+	for (var i = 0; i < colors.length; i++) {
+		var el = document.createElement('div');
+		el.id = colors[i].name;
+		el.className = 'colorChoice';
+		var elName = document.createElement('span');
+		elName.innerHTML = colors[i].name;
+		var elSwatch = document.createElement('div');
+		elSwatch.className = 'colorSwatch';
+		elSwatch.style.backgroundColor = '#' + colors[i].hex;
+		el.appendChild(elName);
+		el.appendChild(elSwatch);
+		palette.appendChild(el);
+	}
+}());
