06b207681390e497dad38a5283b33cb342cf2a23
petermalcolm <peter.t.malcolm@gmail.com>
2018-01-06 14:15:50 -0500

    buttons for UI, update userColor

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@
 (function iife(){
 	console.log('ready to mix colors! ' + colors.length);
 
+	var goalColor = 'lightblue';
+
 	function initPalette() {
 		var palette = document.querySelector('#palette');
 		palette.innerHTML ='<h2>Your Colors</h2>';
@@ -19,13 +21,48 @@
 			var elSwatch = document.createElement('div');
 			elSwatch.className = 'colorSwatch';
 			elSwatch.style.backgroundColor = '#' + colors[i].hex;
+			eventListenerForSwatch(elSwatch,colors[i].hex);
 			el.appendChild(elName);
 			el.appendChild(elSwatch);
 			palette.appendChild(el);
 		}
 	}
 
+	function eventListenerForSwatch(swatch,hex) {
+		var newUserColor = '#' + hex;
+		swatch.addEventListener('click', function(e) {
+			updateUserColor(newUserColor);
+		})
+	}
+
+	function updateUserColor(newColor) {
+		// TODO: mix existing color with newColor
+		setUserColor(newColor);
+	}
+
+	function initGoalColor() {
+		setGoalColor(goalColor);
+	}
+
+	function nextColor(e) {
+		setGoalColor('yellow');
+	}
+
+	function setGoalColor(newColor) {
+		document.querySelector('#goal').style.backgroundColor = newColor;
+	}
+
+	function setUserColor(newColor) {
+		document.querySelector('#userColor').style.backgroundColor = newColor;
+	}
+
 	document.addEventListener("DOMContentLoaded", function(event) { 
 		initPalette();
+		initGoalColor();
 	});
+
+	document.querySelector('#nextColor').addEventListener('click', function(e) {
+		nextColor(e);
+	});
+
 }());
